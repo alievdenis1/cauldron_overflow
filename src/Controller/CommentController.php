0ca06a12692767ed286ef8ebe68d02f6a2819ab4
denis <denisaliev7@gmail.com>
2021-01-12 15:34:59 +0500

    we are ROCKING this Symfony thing

--- src/Controller/CommentController.php
+++ src/Controller/CommentController.php
@@ -5,18 +5,22 @@
 
     use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
     use Symfony\Component\Routing\Annotation\Route;
+    use Psr\Log\LoggerInterface;
 
     class CommentController extends AbstractController
     {
         /**
-         * @Route("/comments/{id}/vote/{direction}")
+         * @Route("/comments/{id}/vote/{direction<up|down>}", methods="POST")
          */
-        public function commentVote($id, $direction)
+        public function commentVote($id, $direction, LoggerInterface $logger)
         {
             if ($direction === 'up') {
                 $currentVoteCount = rand(7, 100);
+                $logger->info('Voting up!');
             } else {
                 $currentVoteCount = rand(0, 5);
+                $logger->info('Voting down!');
+
             }
 
             return $this->json(['votes' => $currentVoteCount]);
