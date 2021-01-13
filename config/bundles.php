5b4a708e293a30f258a4c03d3de9a197e2950b1a
denis <denisaliev7@gmail.com>
2021-01-13 18:36:21 +0500

    chapter18

--- config/bundles.php
+++ config/bundles.php
@@ -6,4 +6,6 @@ return [
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+    Knp\Bundle\MarkdownBundle\KnpMarkdownBundle::class => ['all' => true],
 ];
--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -7,13 +7,22 @@
     use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
     use Symfony\Component\HttpFoundation\Response;
     use Symfony\Component\Routing\Annotation\Route;
+    use Twig\Environment;
+    use App\Service\MarkdownHelper;
+
 
     class QuestionController extends AbstractController
     {
+        private $isDebug;
+
+        public function __construct(bool $isDebug)
+        {
+            $this->isDebug = $isDebug;
+        }
         /**
          * @Route("/", name="app_homepage")
          */
-        public function homepage()
+        public function homepage(Environment $twigEnvironment)
         {
             return $this->render('question/homepage.html.twig');
         }
@@ -21,17 +30,24 @@
         /**
          * @Route("/questions/{slug}", name="app_question_show")
          */
-        public function show($slug)
-        {
+        public function show($slug,  MarkdownHelper $markdownHelper)
+        {dump($this->isDebug);
             $answers = [
-                'Make sure your cat is sitting purrrfectly still ?',
+                'Make sure your cat is sitting `purrrfectly` still ?',
                 'Honestly, I like furry shoes better than MY cat',
                 'Maybe... try saying the spell backwards?',
             ];
+            $questionText = 'I\'ve been turned into a cat, any thoughts on how to turn back? While I\'m **adorable**, I don\'t really care for cat food.';
+
+
+            $parsedQuestionText = $markdownHelper->parse($questionText);
+
             return $this->render('question/show.html.twig', [
                 'question' => ucwords(str_replace('-', ' ', $slug)),
                 'answers' => $answers,
+                'questionText' => $parsedQuestionText,
             ]);
         }
+
 
     }
